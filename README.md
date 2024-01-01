abb2b6e72c73c25b2de48e670d3925c0c7881b63
Mike Sheliga <msheliga1@gmail.com>
2024-01-01 11:57:37 -0500

    Many updates index, js. fetches-localStorage

--- README.md
+++ README.md
@@ -11,57 +11,57 @@ Link to screenshot of deployed github.io site. ![HW 6 Fetch APIs GitHub Deployed
 Link to screenshot of 2. ![HW 6 Fetch APIs GitHub Results Screenshot](./assets/images/ResultsScreenshot.PNG)   
 
 ## Project Goals  
-Use the 3rd party openweatehrmap API, jQuery, fetch, local storage and a the BootStrap CSS framework, to create a 5-day weather application. 
-The user will type a city name into a form, upon submit the API will be used to get latitude-longitude for the city, and the 
+Use the 3rd party openweatehrmap API, jQuery, fetch, local storage and a the BootStrap CSS framework, to create a 5-day weather application.  
+The user will type a city name into a form, upon submit the API will be used to get latitude-longitude for the city, and the  
 API will then be used to get the 5 day weather forecast. If the city exists it will be saved to local storage.    
-A sample API call: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} 2603ab2cb7ddafbe94e3a350338be59b
-Note that using this key in a public repo effectively makes the openweathermap key public. 
+A sample API call: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} 2603ab2cb7ddafbe94e3a350338be59b  
+Note that using this key in a public repo effectively makes the openweathermap key public.  
 
-==============================================================================================================================
-## Technical Project Details 
-==============================================================================================================================
-## Github:  
-    Create Repo (github, repositories => New)  
-        - Dont Make this a shared repo. 
-    Copy directories and sample files from prior project (or create from scratch). 
-        -- No starter code. No need for copying one file at a time via command line. Worked well.
+==============================================================================================================================  
+## Technical Project Details  
+==============================================================================================================================  
+## Github:   
+    Create Repo (github, repositories => New)   
+        - Dont Make this a shared repo.  
+    Copy directories and sample files from prior project (or create from scratch).  
+        -- No starter code. No need for copying one file at a time via command line. Worked well.  
         -- Alternate: Go to Demo (root) folder, download zip, moving to local repo, unzip - likely fastest method.     
         -- Could not find a better way to copy over all files.    
-        -- OR ... create HTML, CSS and javascript, etc. from scratch or sample files ...  
-    Branches (Optional for single programmer projects)
-        - Could do work in branches. (new branch inside gitHub)  
+        -- OR ... create HTML, CSS and javascript, etc. from scratch or sample files ...   
+    Branches (Optional for single programmer projects)  
+        - Could do work in branches. (new branch inside gitHub)    
         - All branch names will begin with the initials of the main person working on the branch.  
         - Must update local repo after adding a branch  
-        - Switch to branch: From cmd line git switch <branchname>  
+        - Switch to branch: From cmd line git switch <branchname>   
         - Once changes committed, git push origin <branchname>  
-            - for pushing to remote test branch: git push origin local_branch:remote_branch
-        - Issue a pull request in gitHub.
-        - Click "Pull Requests" in top menu bar (3rd from left).
-        - Click "review Required" in small font below pull request name.
+            - for pushing to remote test branch: git push origin local_branch:remote_branch  
+        - Issue a pull request in gitHub.  
+        - Click "Pull Requests" in top menu bar (3rd from left).  
+        - Click "review Required" in small font below pull request name.  
         - You may approve your own request.  
     Clone to local machine (Copy https, then git clone paste).    
     Create a nice long READ.md file!!  (Modify prior projects.)   
-    Commit and push files back to gitHub branch. (For multi-programming: Issue pull request, approve, merge). 
-    Deploy code (Settings...CodeAndAnimation->Pages on left, GitHub Pages->Branch->main, save)
-        - Deployed code name always msheliga1/github.io/RepoName !!
-    Make Sure it Works  
+    Commit and push files back to gitHub branch. (For multi-programming: Issue pull request, approve, merge).  
+    Deploy code (Settings...CodeAndAnimation->Pages on left, GitHub Pages->Branch->main, save)  
+        - Deployed code name always msheliga1/github.io/RepoName !!  
+    Make Sure it Works   
     Insert Screenshot X2 of deployment into readme file.  
   
 ## Tools and Technologies Used - HTML, CSS Bulma Framework and JQuery   
-    Github - Could use branches
-    Create/Modify HTML 
-    Modals - May use, may not
-    CSS Framwork Bootstrap
-    Javascript/JQuery 
-    Local Storage
-    Agile - Try to assign a little work at a time. 
+    Github - Could use branches  
+    Create/Modify HTML  
+    Modals - May use, may not  
+    CSS Framwork Bootstrap  
+    Javascript/JQuery   
+    Local Storage   
+    Agile - Try to assign a little work at a time.  
 
-## Acceptance Criteria  
------------------------
-weather outlook for multiple cities
-weather dashboard with form inputs, search for a city => current and future conditions for city and city is added to the search history
-Current weather conditions include: city name, the date, an icon of weather conditions, temperature, humidity, and wind speed
-Future weather conditions include: a 5-day forecast with date, an icon of weather conditions, temperature, wind speed, and humidity
-Click on a city in search history:  presented with current and future conditions for that city
+## Acceptance Criteria   
+-----------------------  
+weather outlook for multiple cities  
+weather dashboard with form inputs, search for a city => current and future conditions for city and city is added to the search history  
+Current weather conditions include: city name, the date, an icon of weather conditions, temperature, humidity, and wind speed  
+Future weather conditions include: a 5-day forecast with date, an icon of weather conditions, temperature, wind speed, and humidity  
+Click on a city in search history:  presented with current and future conditions for that city  
 
 
---
+++ assets/js/source.js
@@ -0,0 +1,224 @@
+// MJS URI hw6 Weather Dashboard 12.30.23 - Due 1.2.24 
+// javascript for index.html file. 
+myLog("File source.js opened and loading .. ");
+
+
+// ----------- Constants and Functions =============================
+const Text_Search_Box = "#text-search-box";
+
+function searchTextButtonClickFunction(ev) {
+    myLog("Starting searchForCity"); 
+    ev.preventDefault();  // keep input values
+    var inputBox = $(Text_Search_Box);
+    var inputText = inputBox.val();
+    myLog("Searching for City " + inputText);
+    if (inputText === null) {
+        alert("Could not get text from text box (null).");
+    } else if (inputText === undefined) {
+        alert("Could not get text from text box (undefined).");
+    } else if (inputText.length === 0) {
+        alert("Text box blank. Try again");
+    } else if (inputText === "Enter City") {
+        alert('"Enter City" is not a valid city! Try again. ') 
+    } else {
+        searchForCity(inputText);
+        if (!locallyStoredValueExists(inputText)) { 
+            storeLocalData(inputText);
+            buttonifyLocalStorage(localStoreDiv, localStorePrefix);
+        }
+    }
+}  // end function searchTextButtonClickFunction(ev)
+
+function locallyStoredButtonClickFunction(ev) {
+    var cityName = ev.target.val(); 
+    myLog("A locally stored button has been clicked! " + cityName);
+    searchForCity(cityName);
+}
+
+// Clear the local storage - MJS 12.29.23
+function clearLocalStorageButtonClickFunction() {
+    myLog("clearing local storage button click function.");
+    clearLocalStorage(localStorePrefix);
+    buttonifyLocalStorage(localStoreDiv, localStorePrefix); // will remove old local storage
+}
+
+// search for a city using open weather api. MJS 12.31.23
+// Typical call - api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} 2603ab2cb7ddafbe94e3a350338be59b 
+// Citu call - https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+function searchForCity(cityName) {
+    myLog("Searching for City " + cityName);
+    setCityDateInDom(cityName);
+    const apiUrlBase = "https://api.openweathermap.org/data/2.5/";
+    const weatherText = "weather?";
+    var cityQS = "q="+cityName;
+    var appIdQS = "&appid=" + "2603ab2cb7ddafbe94e3a350338be59b";
+    var requestUrl = apiUrlBase + weatherText + cityQS + appIdQS; 
+
+    const forecastText = "forecast?";
+    myLog("Open weather map city request url: " + requestUrl);
+
+
+    fetch(requestUrl)
+        .then(function (response) {
+            console.log("Weather API Response is: " + response + " Status: " + response.status);
+            if (!response.ok) {
+                myLog("Bad response status ... returning status. ");
+                // whether you set this or not, whether 200 or 404 you always get a .cod in the result.
+                response.textContent = response.status;  // or could return, and check if data undefined below.
+            }
+            return response.json(); // cod=404 if bad, 200 if good, etc.
+        })
+        .then(function (data) {
+            console.log(data);  // this shows up in console as collaspable-expandable. Better than stringify.
+            if (data.cod >= 300) {
+                myLog("No weather results found for " + cityName + " ... returning");
+                // addNoResulsFoundToDom("No movies found for superhero " + heroName);
+                return;
+            }
+            // console.log("Data - JSON stringified is: " + JSON.stringify(data));
+            var coord = data.coord;
+            var lat = coord.lat;
+            var lon = coord.lon; 
+            var latlonQS = "lat=" + lat + "&lon=" + lon; 
+            var requestUrl2 = apiUrlBase + forecastText + latlonQS + appIdQS;
+            myLog("Open weather map lat-lon request url: " + requestUrl2);
+
+            fetch(requestUrl2)
+            .then(function (response) {
+                console.log("Weather API forecast Response is: " + response + " Status: " + response.status);
+                if (!response.ok) {
+                    response.textContent = response.status; 
+                }
+                return response.json(); // returns a promise
+            })
+            .then(function (data) {
+                if (response.cod >= 300) {
+                    myLog("Could not find data for lat-long ");
+                    return;
+                }
+                console.log(data);  // this shows up in console as collaspable-expandable. Better than stringify.
+                if (data.total_results === 0) {
+                            myLog("No weather results found for " + cityName);
+                            // addNoResulsFoundToDom("No movies found for superhero " + heroName);
+                            return;
+                    }
+            })
+            // addMovieSummaryToDom(results);
+
+        });  // end .then-function
+}  // end function searchForCity
+
+// ================= DOM methods ============================================
+// Set the City and todays date in the DOM
+function setCityDateInDom(cityName) {
+    myLog("Setting city-date " + cityName);
+    var cityDateEl = $('#city-date');
+    if (cityDateEl === null || cityDateEl === undefined) {
+        myLog("city-date is undefined or null");
+    }
+    var date = dayjs().format("M-D-YYYY");
+    myLog("dayjs date is " + date);
+    cityDateEl.text("cityName  (" + date + ")");
+}
+
+// ================  Local store class =============================
+var localStorePrefix = "CityName_";     // ideally these would be external to the class.
+var localStoreDiv = "#saved-data-div";  // DOM div to store the data
+
+// create an object that links a local storage prefix (ie. CityName_) to a html div (ie saved-data-div). MJS 12.30.23
+// Ideally this would be an immutable object
+function createLocalStorageLink(prefix, div) {
+    var link = {};
+    link.localStorePrefix = prefix;
+    link.localStoreDiv = div; 
+    return link;
+} 
+
+// // load Local Data beginning with prefix into div. MJS 12.30.23
+function storeLocalData(value) {
+    myLog("Storing local data " + value);
+    var count = getLocallyStoredDataCount(localStorePrefix); 
+    localStorage.setItem(localStorePrefix+(count+1), value);
+}
+
+// load Local Data beginning with prefix into div buttons. MJS 12.30.23
+function buttonifyLocalStorage(elementId, prefix) {
+    var div = $(localStoreDiv); 
+    div = $('#saved-data-div');
+    div.empty(); // div.innerHTML = "";  // wipe out past values.
+    myLog("The jQuery div is " + div + " Str " + JSON.stringify(div)); 
+    var count = getLocallyStoredDataCount(prefix);
+    for (var i=1; i<=count; i++) {
+        var value = localStorage.getItem(prefix+i);
+        myLog("Creating button " + i + ". " + value);
+        var row = $('<div/>');
+        row.attr("class", "row");  // rows display on a seperate line
+        div.append(row);
+        var btn = $('<button/>');
+        btn.attr("class", "btn bg-secondary my-1 ml-3");
+        btn.attr("id", value);
+        btn.attr("onClick", "locallyStoredButtonClickFunction(event)");
+        btn.text(value);
+        row.append(btn);        
+    }
+    myLog("Done loadingLocalData");
+    var row = $("<div/>");
+    row.attr("class", "row");
+    div.append(row);
+    var btn = $("<btn/>");
+    btn.attr("class", "btn bg-warning my-1 ml-3");
+    btn.attr("onclick", "clearLocalStorageButtonClickFunction()"); 
+    btn.text("Clear Cities");
+    row.append(btn);
+}  // end function buttonifyLocalStorage
+
+// Return the number of locally stored values (prefix_index) beginning at 1. MJS 12.30.23.  
+// If prefix_1 thru prefix_10 exists, but prefix_5 is missing 4 will be returned.
+function getLocallyStoredDataCount(prefix) {
+    done = false;
+    var i = 1;
+    while (! done) {
+        var keyStr = prefix + i;
+        var value = localStorage.getItem(keyStr);
+        if (value === null || value.length === 0) {
+            done = true;
+        } else {
+            i++;
+        }
+    } // end while not done
+    return (i - 1);
+} // end function getLocallyStoredDataCouont
+ 
+// see if the key is in local storage for the input prefix.  MJS 12.30.23 
+function locallyStoredValueExists(value) {
+    console.log("Searching for locally stored value .... " + value);
+    var count = getLocallyStoredDataCount(localStorePrefix);
+    for (i=1; i<=count; i++ ) {
+        if (localStorage.getItem(localStorePrefix+i) === value) {  // no trim or lowerCase
+            return true;
+        }
+    }
+    return false;
+}  // end function locallyStoredValueExists
+
+// load Local Data beginning with prefix into div buttons. MJS 12.30.23
+function clearLocalStorage(prefix) {
+    var count = getLocallyStoredDataCount(prefix);
+    for (var i=1; i<=count; i++) {
+        localStorage.removeItem(prefix+i);
+        myLog("Deleting local storage " + prefix + i);        
+    }
+    myLog("Done clearingLocalStorage");
+}  // end function clearLocalStorage
+
+
+// Accesory Routines 
+// ------------------------------------------------
+function myLog(str) {  // to easily turn debugging on/off
+    console.log(str); 
+}
+
+// ============== code to execute when loading (const must be declared first) =======
+// storeLocalData(localStorePrefix, 1, "London");
+buttonifyLocalStorage(localStoreDiv, localStorePrefix); 
+myLog("Done loading scritp.js page. ");
